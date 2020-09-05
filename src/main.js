a85b5f1b4e91501f63a841a290f8c49e154be049
david <david@davids-imac.home>
2020-09-05 22:30:38 +0100

    Main electron application

---
+++ src/main.js
@@ -0,0 +1,84 @@
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+require("electron-reload")(__dirname);
+
+const { createWriteStream } = require("fs");
+const ytdl = require("ytdl-core");
+
+const p = require("./drpc");
+const { getInfo } = require("ytdl-core");
+p.presence
+
+const database = require("./db");
+const db = database.db;
+
+const options = {
+    width: 800,
+    height: 600,
+    center: true,
+    icon: __dirname + "./icons/ytdl_icon.icns",
+    webPreferences: {
+        nodeIntegration: true,
+        devTools: true
+    },
+};
+
+function windowCreate() {
+    const window = new BrowserWindow(options);
+
+    window.loadFile("./web/index.html");
+};
+
+// Method is called when Electron has finished initialisation.
+app.whenReady().then(windowCreate);
+
+// Quit when windows are closed unless on macOS, where it it common that windows will stay active
+// unless Cmd+Q is used.
+app.on("window-all-closed", () => {
+    // If OS is not macOS quit window
+    if (process.platform !== "darwin") {
+        app.quit();
+    };
+});
+
+app.on("activate", () => {
+    // If no windows, create window.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        windowCreate();
+    };
+});
+
+// Receive from index.html
+ipcMain.on("download_mp4", (event, arg) => {
+   try {
+       ytdl(arg).pipe(createWriteStream("video.mp4"))
+   } catch (error) {
+       console.error("Invalid URL", error);
+   };
+});
+
+ipcMain.on("download_mp3", (event, arg) => {
+    try {
+        // Async always returns a promise. async/await syntax allows for working with promises more comfortably.
+        const title = ytdl.getInfo(arg).then((info) => {
+            // Put execution mode into serialized, this means that at most only one statement obj can execute at a time.
+            // Other statements wait in a queue until previous statements are executed.
+            // Callbacks are called immediately
+            db.serialize(() => {
+                db.run("CREATE TABLE IF NOT EXISTS video_info (id int primary key, text title)");
+                const stmt = db.prepare("INSERT INTO video_info VALUES (?, ?)");
+                stmt.run(info.videoDetails.title);
+                stmt.finalize();
+            });
+            db.close();
+            return info.videoDetails.title;
+        });
+
+        title.then((t) => {
+            ytdl(arg, {
+                filter: "audioonly"
+            }).pipe(createWriteStream(`${t}.mp3`));
+        });
+    } catch (error) {
+        console.error("Invalid URL", error)
+    };
+});
